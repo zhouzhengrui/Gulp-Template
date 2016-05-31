2db22a468887fba01c0dcf1b74133aef490a8666
zhouzhengrui <zhouzhengrui@gmail.com>
2016-05-31 17:06:34 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,15 @@
-// 引入gulp及组件
-var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    fileinclude = require('gulp-file-include'),
-    browserSync = require('browser-sync').create();
+// 引入gulp, plugins, config
+var gulp            = require('gulp'),
+    sass            = require('gulp-sass'),
+    tinyPNG         = require('gulp-tinypng'),
+    fileInclude     = require('gulp-file-include'),
+    browserSync     = require('browser-sync').create(),
+    config          = require('./config.json');
 
-// html任务, 执行file include组件
+// html任务, 执行fileinclude组件
 gulp.task('html', function() {
     gulp.src(['./src/*.html'])
-        .pipe(fileinclude({
+        .pipe(fileInclude({
             prefix: '@@',
             basepath: '@file'
         }))
@@ -16,20 +18,33 @@ gulp.task('html', function() {
 
 // sass编译任务, css注入
 gulp.task('sass', function() {
-    return gulp.src('./src/sass/*.scss')
+    return gulp.src('./src/**/*.scss')
         .pipe(sass({
-            outputStyle: 'compressed'
+            outputStyle: 'compressed'   // nested, compact, expanded, compressed
         }).on('error', sass.logError))
+
         .pipe(gulp.dest('./dist/css'))
         .pipe(browserSync.reload({
             stream: true
         }));
 });
 
+// 图片压缩任务
+gulp.task('tinyPNG', function() {
+    gulp.src('./src/img/*.{png,jpg}')
+        .pipe(tingPNG('s0X_ATRE4UrEYqtUSDTHkhEQ_Tj93IAn'))
+        .pipe(gulp.dest('dist'));
+});
+
 // 默认任务
 gulp.task('default', function() {
     gulp.run('html', 'sass', 'server');
 });
+
+// 后期处理任务
+gulp.task('build', function() {
+    gulp.run('tunyPNG');
+})
 
 // 静态服务器, 监听任务, 浏览器重载
 gulp.task('server', function() {
@@ -40,5 +55,5 @@ gulp.task('server', function() {
     });
     gulp.watch('./src/**/*.html', ['html']);
     gulp.watch("./src/**/*.scss", ['sass']);
-    gulp.watch("./dist/*.html").on('change', browserSync.reload);
+    gulp.watch("./dist/**/*.html").on('change', browserSync.reload);
 });
